c57bfa1299a470006027449a125203982b7e00ef
Adnan Ahmed <idnan.se@gmail.com>
2018-10-07 00:29:54 +0400

    Generate hash

--- cmd/server/helpers.go
+++ cmd/server/helpers.go
@@ -12,3 +12,41 @@ func Base64Decode(str string) string {
 	data, _ := base64.StdEncoding.DecodeString(str)
 	return string(data)
 }
+
+func GenerateHash(seed interface{}) string {
+
+	series := []string{
+		"1", "2", "3", "4", "5", "6", "7", "8", "9",
+		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+		"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	}
+
+	seedStr := seed.(string)
+	charsCount := len(seedStr)
+
+	if len(seedStr) == 0 {
+		return series[0]
+	}
+
+	lastChar := string(seedStr[charsCount-1])
+
+	// find last char index in the series
+	// this will help us in finding the next char from series
+	i := 0
+	for k, v := range series {
+		if lastChar == v {
+			i = k
+			break
+		}
+	}
+
+	// if the last has reached the end of series then append first char
+	if lastChar == "z" {
+		return seedStr + series[0]
+	}
+
+	// append the next char from series to the last hash-1
+	return seedStr[:charsCount-1] + series[i+1]
+}
--- cmd/server/helpers_test.go
+++ cmd/server/helpers_test.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGenerateHash(t *testing.T) {
+	data := map[string]string{
+		"":    "1",
+		"8":   "9",
+		"9":   "A",
+		"A":   "B",
+		"X":   "Y",
+		"Z":   "a",
+		"y":   "z",
+		"z":   "z1",
+		"z1":  "z2",
+		"z8":  "z9",
+		"z9":  "zA",
+		"zX":  "zY",
+		"zY":  "zZ",
+		"za":  "zb",
+		"zy":  "zz",
+		"zz":  "zz1",
+		"zz1": "zz2",
+		"zzA": "zzB",
+		"zzY": "zzZ",
+		"zzy": "zzz",
+	}
+
+	for seed, expected := range data {
+		actual := GenerateHash(seed)
+		assert.Equal(t, expected, actual)
+	}
+}
